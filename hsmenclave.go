8090478be4284392f0209158bebafc3bc6173fb9
Sumner Evans <me@sumnerevans.com>
2022-11-22 23:54:43 -0700

    hsmenclave: wrap functions
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

---
+++ hsmenclave.go
@@ -0,0 +1,74 @@
+package libsignalgo
+
+/*
+#cgo LDFLAGS: ./libsignal/target/release/libsignal_ffi.a -ldl
+#include "./libsignal/libsignal-ffi.h"
+*/
+import "C"
+import (
+	"runtime"
+	"unsafe"
+)
+
+type HSMEnclaveClient struct {
+	ptr *C.SignalHsmEnclaveClient
+}
+
+func wrapHSMEnclaveClient(ptr *C.SignalHsmEnclaveClient) *HSMEnclaveClient {
+	hsmEnclaveClient := &HSMEnclaveClient{ptr: ptr}
+	runtime.SetFinalizer(hsmEnclaveClient, (*HSMEnclaveClient).Destroy)
+	return hsmEnclaveClient
+}
+
+func NewHSMEnclaveClient(trustedPublicKey, trustedCodeHashes []byte) (*HSMEnclaveClient, error) {
+	var cds *C.SignalHsmEnclaveClient
+	signalFfiError := C.signal_hsm_enclave_client_new(&cds, BytesToBuffer(trustedPublicKey), BytesToBuffer(trustedCodeHashes))
+	if signalFfiError != nil {
+		return nil, wrapError(signalFfiError)
+	}
+	return wrapHSMEnclaveClient(cds), nil
+}
+
+func (hsm *HSMEnclaveClient) Destroy() error {
+	runtime.SetFinalizer(hsm, nil)
+	signalFfiError := C.signal_hsm_enclave_client_destroy(hsm.ptr)
+	if signalFfiError != nil {
+		return wrapError(signalFfiError)
+	}
+	return nil
+}
+
+func (hsm *HSMEnclaveClient) InitialRequest() ([]byte, error) {
+	var resp *C.uchar
+	var length C.ulong
+	signalFfiError := C.signal_hsm_enclave_client_initial_request(&resp, &length, hsm.ptr)
+	if signalFfiError != nil {
+		return nil, wrapError(signalFfiError)
+	}
+	return C.GoBytes(unsafe.Pointer(resp), C.int(length)), nil
+}
+
+func (hsm *HSMEnclaveClient) CompleteHandshake(handshakeReceived []byte) error {
+	signalFfiError := C.signal_hsm_enclave_client_complete_handshake(hsm.ptr, BytesToBuffer(handshakeReceived))
+	return wrapError(signalFfiError)
+}
+
+func (hsm *HSMEnclaveClient) EstablishedSend(plaintext []byte) ([]byte, error) {
+	var resp *C.uchar
+	var length C.ulong
+	signalFfiError := C.signal_hsm_enclave_client_established_send(&resp, &length, hsm.ptr, BytesToBuffer(plaintext))
+	if signalFfiError != nil {
+		return nil, wrapError(signalFfiError)
+	}
+	return C.GoBytes(unsafe.Pointer(resp), C.int(length)), nil
+}
+
+func (cds *HSMEnclaveClient) EstablishedReceive(ciphertext []byte) ([]byte, error) {
+	var resp *C.uchar
+	var length C.ulong
+	signalFfiError := C.signal_hsm_enclave_client_established_recv(&resp, &length, cds.ptr, BytesToBuffer(ciphertext))
+	if signalFfiError != nil {
+		return nil, wrapError(signalFfiError)
+	}
+	return C.GoBytes(unsafe.Pointer(resp), C.int(length)), nil
+}
