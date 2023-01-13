98091efb0d9ee5afbe45f5fa4919ae70942dbba8
Sumner Evans <me@sumnerevans.com>
2023-01-12 21:22:36 -0700

    add more tests
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

---
+++ identitykey_test.go
@@ -0,0 +1,22 @@
+package libsignalgo_test
+
+import (
+	"testing"
+
+	"github.com/beeper/libsignalgo"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSignAlternateIdentity(t *testing.T) {
+	primary, err := libsignalgo.GenerateIdentityKeyPair()
+	assert.NoError(t, err)
+	secondary, err := libsignalgo.GenerateIdentityKeyPair()
+	assert.NoError(t, err)
+
+	signature, err := secondary.SignAlternateIdentity(primary.GetIdentityKey())
+	assert.NoError(t, err)
+
+	verified, err :=secondary.GetIdentityKey().VerifyAlternateIdentity(primary.GetIdentityKey(), signature)
+	assert.NoError(t, err)
+	assert.True(t, verified)
+}
